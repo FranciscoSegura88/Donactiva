c53cacf125434af78fe25250510310bc10455f27
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2024-10-09 15:17:32 -0600

    Genere el proyecto de spring boot

---
+++ proyecto/src/test/java/com/donactiva/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donactiva.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
