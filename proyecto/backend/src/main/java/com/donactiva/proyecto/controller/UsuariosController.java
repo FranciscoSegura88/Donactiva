35a3e7f2a01d3697866733566ea37a97a8841f1c
FranciscoSegura88 <125767908+FranciscoSegura88@users.noreply.github.com>
2024-10-13 17:30:36 -0600

    cambios en la estructura

---
+++ proyecto/backend/src/main/java/com/donactiva/proyecto/controller/UsuariosController.java
@@ -0,0 +1,36 @@
+package com.donactiva.proyecto.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.donactiva.proyecto.model.Usuarios;
+import com.donactiva.proyecto.service.UsuariosService;
+
+@RestController
+@RequestMapping("/api")
+public class UsuariosController {
+
+    @Autowired
+    private UsuariosService usuarioService;
+
+    @GetMapping("/LogIn")
+    public String obtenerUsuarioPorEmail(@RequestParam String correo, @RequestParam String contraseña) {
+        
+        Usuarios usuario = usuarioService.obtenerUsuarioPorCorreo(correo);
+        if (usuario.getContraseña().equals(contraseña)){
+            return "Inicio de sesion exitoso.";
+        }
+        
+        return "Credenciales invalidas.";
+    }
+
+    @PostMapping("/signUp")
+    public void guardarUsuario(@RequestBody Usuarios usuario){
+        usuarioService.guardarUsuario(usuario);
+    }
+}
