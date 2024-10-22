9e4c096d553d5ba55d40a6e81f3f7e0ef63f867d
unknown <guegoniado09@hotmail.com>
2024-10-21 19:50:05 -0600

    Sign up y ruteos simples

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
     {path: 'main', component: MainComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'sign-up', component: SignUpComponent}
 ];
