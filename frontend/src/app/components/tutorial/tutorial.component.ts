632bf3b64788bc569066208e43548f6ecda2bb90
unknown <guegoniado09@hotmail.com>
2024-10-21 21:16:34 -0600

    componentes tutorial y forgotPswd

---
+++ frontend/src/app/components/tutorial/tutorial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutoial',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutoial.component.html',
+  styleUrl: './tutoial.component.css'
+})
+export class TutoialComponent {
+
+}
