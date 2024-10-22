45834ba60cd76d3c478f73366d1dc44f9d97af7b
unknown <guegoniado09@hotmail.com>
2024-10-21 22:46:40 -0600

    Modal de tutorial y forgot password

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { TutorialComponent } from './components/tutorial/tutorial.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'sign-up', component: SignUpComponent},
-    {path: 'forgot-password', component : ForgotPasswordComponent}
+    {path: 'forgot-password', component : ForgotPasswordComponent},
+    {path: 'tutorial', component : TutorialComponent}
 ];
--- frontend/src/app/components/tutorial/tutorial.component.ts
+++ frontend/src/app/components/tutorial/tutorial.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+declare var bootstrap: any;
 
 @Component({
-  selector: 'app-tutoial',
+  selector: 'app-tutorial',
   standalone: true,
-  imports: [],
-  templateUrl: './tutoial.component.html',
-  styleUrl: './tutoial.component.css'
+  imports: [RouterLink],
+  templateUrl: './tutorial.component.html',
+  styleUrl: './tutorial.component.css'
 })
-export class TutoialComponent {
 
+export class TutorialComponent {
+  
+  @ViewChild('exampleModalLong', { static: false }) modal!: ElementRef;
+
+  abrirModal() {
+    const modal = new bootstrap.Modal(this.modal.nativeElement);
+    modal.show();
+  }
+
+  cerrarModal() {
+    const modal = new bootstrap.Modal(this.modal.nativeElement);
+    modal.hide();
+  }
 }
+
+
